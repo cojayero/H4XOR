36c43be7f04f0f2446477dcf57f5d2e1a2c74e38
Baldomero Fernandez <f_baldomero@hotmail.com>
2022-05-16 18:23:13 +0200

    Initial Commit

---
+++ H4XOR/H4XORApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4XORApp.swift
+//  H4XOR
+//
+//  Created by Baldomero Fernandez Manzano on 16/5/22.
+//
+
+import SwiftUI
+
+@main
+struct H4XORApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
